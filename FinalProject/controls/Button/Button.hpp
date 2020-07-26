142f41325ea79cc01e961357fdec5a51dd9ccc1f
Tal Adivi <39825719+TalAdivi@users.noreply.github.com>
2020-07-26 13:09:06 +0300

    TextBox and simple Button working with panel

--- FinalProject/controls/Button/Button.hpp
+++ FinalProject/controls/Button/Button.hpp
@@ -9,4 +9,5 @@ public:
     Button(short left, short top, short width, BorderDrawer *border, Color textColor, Color BgColor, string title);
     // ~Button();
     void keyDown(int keyCode, char character);
+    bool canGetFocus(){ return true; }
 };
